6e005719f8f9ec465c75d662f2dab3366908643a
fmel025 <00018720@uca.edu.sv>
2024-05-28 22:46:43 -0600

    🔧 Add global pipes at app component

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('[ARI] - API documentation')
@@ -11,9 +13,12 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
 
+  app.useGlobalPipes(new ValidationPipe());
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
 }
+
 bootstrap();
