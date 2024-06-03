d4ff0ce5f0d4a7c70e9aea0e52a191650aeb5b7c
fmel025 <00018720@uca.edu.sv>
2024-06-02 20:24:17 -0600

    ✨ Add upload csv data example

--- backend/src/upload/dto/upload.dto.ts
+++ backend/src/upload/dto/upload.dto.ts
@@ -2,19 +2,24 @@ import { ApiProperty } from '@nestjs/swagger';
 import { ArrayNotEmpty, IsArray, IsNotEmpty, IsString } from 'class-validator';
 
 export class UploadCsvDataDto {
-  @ApiProperty({ type: [String] })
+  @ApiProperty({
+    type: [String],
+    example: [
+      '23212332-2;Jose Roberto;Hernandez Mendez;2312321312321;GOLD;25252525;((2.1 1, 2 3))',
+    ],
+  })
   @IsArray()
   @ArrayNotEmpty()
   @IsString({ each: true })
   @IsNotEmpty({ each: true })
   data: string[];
 
-  @ApiProperty()
+  @ApiProperty({ example: 'super-secret-key' })
   @IsString()
   @IsNotEmpty()
   cipherKey: string;
 
-  @ApiProperty()
+  @ApiProperty({ example: ';' })
   @IsString()
   @IsNotEmpty()
   delimiter: string;
