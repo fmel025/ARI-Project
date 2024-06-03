7ae80edc361130cc69420ce2bb54ccb4831bf568
fmel025 <00018720@uca.edu.sv>
2024-06-02 20:25:37 -0600

    ✨ Add endpoint to parse json data to file

--- backend/src/upload/controllers/upload.controller.ts
+++ backend/src/upload/controllers/upload.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Res } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
 
 import { UploadService } from '../services/upload.service';
 import { UploadCsvDataDto } from '../dto/upload.dto';
+import { GenericDto } from '@Upload/dto/generic.dto';
 
 @Controller('upload')
 @ApiTags('Upload')
@@ -17,5 +19,20 @@ export class UploadController {
   @Post('parse')
   async parseCsvDataToJson(@Body() uploadDto: UploadCsvDataDto) {
     return this.uploadService.processCsvData(uploadDto);
+  }
+
+  @ApiOperation({
+    description: 'Parse json data to file',
+    summary: 'Use it to parse a json string to a json file',
+  })
+  @Post('download')
+  downloadJson(@Body() genericDto: GenericDto, @Res() res: Response) {
+    // Convertir el JSON a un string
+    const jsonString = JSON.stringify(genericDto.data, null, 2);
+
+    // Configurar la respuesta para que el cliente la descargue como archivo
+    res.setHeader('Content-Disposition', 'attachment; filename=data.json');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(jsonString);
   }
 }
