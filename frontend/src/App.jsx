6b55038ceb51bb18f5c2e168dc9ae492c579c589
Samuel581 <00023020@uca.edu.sv>
2024-06-09 18:45:12 -0600

    :sparkles: Integrate /upload/parse endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,22 @@
 import { useState } from 'react';
-import Papa from 'papaparse';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import uploadCsv from './helpers/uploadCsv';
+import axios from 'axios';
 
 function App() {
-  const [tableData, setTableData] = useState([]);
-  const [headers, setHeaders] = useState([]);
   const [csvLines, setCsvLines] = useState([]);
   const [separatorValue, setSeparatorValue] = useState('');
   const [passwordValue, setPasswordValue] = useState('');
+  const [jsonContent, setJsonContent] = useState(null);
 
   const onSeparatorChange = ({ target }) => {
     setSeparatorValue(target.value);
+    console.log(separatorValue)
   };
 
   const onPasswordChange = ({ target }) => {
     setPasswordValue(target.value);
+    console.log(passwordValue)
   };
 
   const onInputClick = (event) => {
@@ -26,47 +26,56 @@ function App() {
   const handleCsvView = (event) => {
     const file = event.target.files[0];
     if (file) {
-      Papa.parse(file, {
-        complete: (result) => {
-          const data = result.data;
-          setHeaders(data[0]); // Asume que la primera fila es el encabezado
-          let rows = data.slice(1);
-          setTableData(rows); // Resto de las filas como contenido
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const text = e.target.result;
+        const lines = text.split('\n')
+          .map(line => line.split(','))
+          .filter(line => line.some(cell => cell.trim() !== "")); // Filtra las líneas vacías
+        let rows = lines.slice(1);
+        let linesArray = rows.map(row => row.join(','));
 
-          // Array para los datos del CSV
-          let linesArray = rows.map(row => row.join(',')); 
-          setCsvLines(linesArray);
-          console.log(linesArray);
-        },
-        header: false
-      });
+        setCsvLines(linesArray);
+        console.log(linesArray);
+      };
+      reader.readAsText(file);
     }
   };
 
   const handleSubmit = async () => {
     try {
-      const response = await uploadCsv(csvLines, separatorValue, passwordValue);
-      console.log(csvLines, separatorValue, passwordValue)
-      console.log('Response:', response);
+      const response = await axios.post('http://localhost:3000/upload/parse', {
+        data: csvLines,
+        delimiter: separatorValue,
+        cipherKey: passwordValue,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      console.log(csvLines, separatorValue, passwordValue);
+      console.log('Response:', response.data);
+      setJsonContent(response.data);
     } catch (error) {
       console.error('Error sending data to API:', error);
     }
   };
 
+
   return (
-    <div className="w-screen flex items-center justify-center mt-5">
+    <div className="w-full flex items-center justify-evenly mt-5">
       <div className="flex flex-col bg-honeydew rounded-2xl p-6 w-full max-w-4xl shadow-lg">
         <h1 className="font-bold text-3xl pb-5 text-center">Generador de JSON</h1>
         <div className="flex flex-col md:flex-row justify-evenly mb-6 w-full gap-4">
           <div className="flex flex-col justify-center w-full md:w-1/3 mb-4 md:mb-0">
             <label className="font-bold mb-2">Escoja su archivo CSV</label>
-            <input type="file" accept=".csv" onChange={handleCsvView} onClick={onInputClick} className="w-full p-2 border rounded" />
+            <input type="file" className="file-input file-input-bordered w-full max-w-xs" accept=".csv" onChange={handleCsvView} onClick={onInputClick}/>
           </div>
           <div className="flex flex-col justify-center w-full md:w-1/3 mb-4 md:mb-0">
-            <label className="font-bold mb-2">Inserte separador a utilizar</label>
+            <label className="font-bold mb-2">Inserte delimitador a utilizar</label>
             <input
               type="text"
-              className="rounded w-full p-2 border"
+              className="input input-bordered w-full max-w-xs"
               value={separatorValue}
               onChange={onSeparatorChange}
             />
@@ -75,7 +84,7 @@ function App() {
             <label className="font-bold mb-2">Clave / Password</label>
             <input
               type="text"
-              className="rounded w-full p-2 border"
+              className="input input-bordered w-full max-w-xs"
               value={passwordValue}
               onChange={onPasswordChange}
             />
@@ -83,23 +92,21 @@ function App() {
         </div>
         <div className="flex flex-col gap-4 w-full">
           <h2 className="font-bold text-xl mb-2">Contenido del archivo subido CSV</h2>
-          <SyntaxHighlighter languaje='csv' style={dark}>
-            {tableData.map(row => row.join(',')).join('\n')}
+          <SyntaxHighlighter language="csv" style={dark}>
+            {csvLines.join('\n')}
           </SyntaxHighlighter>
         </div>
         <div className="flex flex-col gap-5 mt-5">
           <h2 className="font-bold text-xl mb-2">Contenido del archivo generado JSON</h2>
-           {/* TODO: Here goes the JSON after the API call
-           By now this is dummy data stracted from the CSV */}
           <SyntaxHighlighter language="json" style={dark}>
-            {JSON.stringify({ data: csvLines, cipherKey: passwordValue, delimiter: separatorValue }, null, 2)}
+            {JSON.stringify(jsonContent, null, 2)}
           </SyntaxHighlighter>
         </div>
         <div className="flex justify-center gap-5">
           <button onClick={handleSubmit} className="bg-blue-500 text-white font-bold py-2 px-4 rounded">
             Enviar a la API
           </button>
-            {/* TODO: Here goes handleDownload */}
+          {/* TODO: Here goes handleDownload */}
           <button onClick={null} className="bg-blue-500 text-white font-bold py-2 px-4 rounded">
             Descargar JSON
           </button>
