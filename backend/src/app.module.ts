55d1e76a135a878bb6d7967193f8173010aa2335
fmel025 <00018720@uca.edu.sv>
2024-06-10 23:09:04 -0600

    ✨ Add .env schema validation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 
 import { UploadModule } from '@Upload/upload.module';
 import configuration from '@Configuration/configuration';
+import { schema } from '@Configuration/validation-schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
+      validationSchema: schema,
       isGlobal: true,
     }),
     UploadModule,
