33dc681beedaa5a35b8df2fcc0bf1a70456400e6
Samuel581 <00023020@uca.edu.sv>
2024-06-10 23:15:30 -0600

    💄 Improve UI and presentation of the web

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { dark ,dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
@@ -12,9 +12,9 @@ function App() {
   const [jsonContent, setJsonContent] = useState(null);
   const notifyEmptyFile = () => toast.warning('No se ha seleccionado un archivo CSV');
   const notifyEmptyDelimeter = () => toast.warning('El delimitador solamente puede ser una coma o un punto y coma');
-  const notifyEmptyPassword = () => toast.warning('La clave no puede estar vacía'); 
-  const notifyEmptyUpload1 = () => toast.warning('No se ha subido ningun archivo, no se puede copiar nada');
-  const notifyEmptyUpload2 = () => toast.warning('No se ha subido ningun archivo, no se puede descargar nada');
+  const notifyEmptyPassword = () => toast.warning('La clave no puede estar vacía');
+  const notifyEmptyUpload1 = () => toast.warning('No se ha convertido ningun archivo, no se puede copiar nada');
+  const notifyEmptyUpload2 = () => toast.warning('No se ha convertido ningun archivo, no se puede descargar nada');
 
 
   const onSeparatorChange = ({ target }) => {
@@ -34,7 +34,6 @@ function App() {
     setJsonContent(null);
   };
 
- 
 
   const handleCsvView = (event) => {
     const file = event.target.files[0];
@@ -57,22 +56,22 @@ function App() {
   };
 
   const handleCopy = () => {
-    if(jsonContent==null){
+    if (jsonContent == null) {
       notifyEmptyUpload1();
       return;
     }
     navigator.clipboard.writeText(JSON.stringify(jsonContent, null, 2))
-    .then(() => {
-      toast.success('JSON copiado al portapapeles');
-    })
-    .catch((error) => {
-      console.error('Error copying JSON to clipboard:', error);
-      toast.error('Error al copiar JSON al portapapeles');
-    }); 
+      .then(() => {
+        toast.success('JSON copiado al portapapeles');
+      })
+      .catch((error) => {
+        console.error('Error copying JSON to clipboard:', error);
+        toast.error('Error al copiar JSON al portapapeles');
+      });
   }
 
   const handleDownload = () => {
-    if(jsonContent==null){
+    if (jsonContent == null) {
       notifyEmptyUpload2();
       return;
     }
@@ -90,7 +89,7 @@ function App() {
       notifyEmptyFile();
       return;
     }
-    if (delimiterValue ==! ';' || delimiterValue ==! ',') {
+    if (delimiterValue == ! ';' || delimiterValue == ! ',') {
       notifyEmptyDelimeter();
       return;
     }
@@ -118,54 +117,73 @@ function App() {
 
 
   return (
-    <div className="w-full flex items-center justify-evenly mt-5">
-      <div className="flex flex-col bg-honeydew rounded-2xl p-6 w-full max-w-4xl shadow-lg">
-        <h1 className="font-bold text-3xl pb-5 text-center">Generador de JSON</h1>
-        <div className="flex flex-col md:flex-row justify-evenly mb-6 w-full gap-4">
-          <div className="flex flex-col justify-center w-full md:w-1/3 mb-4 md:mb-0">
-            <label className="font-bold mb-2">Escoja su archivo CSV</label>
-            <input type="file" className="file-input file-input-bordered w-full max-w-xs" accept=".csv" onChange={handleCsvView} onClick={onInputClick}/>
-          </div>
-          <div className="flex flex-col justify-center w-full md:w-1/3 mb-4 md:mb-0">
-            <label className="font-bold mb-2">Inserte delimitador a utilizar</label>
+    <div className="mx-auto max-w-5xl space-y-8 px-4 py-12">
+      <h1 className="font-bold text-5xl text-left">Convertidor de CSV a JSON</h1>
+      <h3 className='text-white font-bold text-xl text-center bg-celestialBlue p-1 rounded-md'>Convierte tus archivos CSV a JSON y 3ncr1pt4 los datos sensibles</h3>
+      <div className="flex flex-col bg-whiteSmoke rounded-2xl p-6 max-w-5xl border shadow-lg">
+        <div className="flex flex-row justify-between  gap-5 mb-6">
+          {/* Text input */}
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text font-bold">Escoja su archivo CSV</span>
+            </div>
             <input
-              type="text"
-              className="input input-bordered w-full max-w-xs"
+              type="file"
+              className="file-input file-input-bordered w-full"
+              accept='.csv'
+              onChange={handleCsvView}
+              onClick={onInputClick} />
+          </label>
+          {/* Text input */}
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text font-bold">Ingrese el delimitador del CSV</span>
+            </div>
+            <input type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full"
               value={delimiterValue}
               onChange={onSeparatorChange}
             />
-          </div>
-          <div className="flex flex-col justify-center w-full md:w-1/3">
-            <label className="font-bold mb-2">Clave / Password</label>
-            <input
-              type="text"
-              className="input input-bordered w-full max-w-xs"
-              value={passwordValue}
-              onChange={onPasswordChange}
-            />
-          </div>
+            <div className="label">
+            </div>
+          </label>
         </div>
+        {/* Text input */}
+        <label className="form-control w-full space-y-2">
+          <div className="label">
+            <span className="label-text font-bold">Ingrese la clave de encriptacion</span>
+          </div>
+          <input type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full"
+            value={passwordValue}
+            onChange={onPasswordChange}
+          />
+          <div className="label">
+          </div>
+        </label>
         <div className="flex flex-col gap-4 w-full">
           <h2 className="font-bold text-xl mb-2">Contenido del archivo subido CSV</h2>
-          <SyntaxHighlighter language="csv" style={dark}>
+          <SyntaxHighlighter language="csv" style={dracula}>
             {rawCsvContent}
           </SyntaxHighlighter>
         </div>
-        <div className="flex flex-col gap-5 mt-5">
+        <button onClick={handleSubmit} className="btn bg-resedaGreen text-white w-3/12">
+          Convertir a JSON
+        </button>
+        <div className="flex flex-col gap-4 w-full">
           <h2 className="font-bold text-xl mb-2">Contenido del archivo generado JSON</h2>
-          <SyntaxHighlighter language="json" style={dark}>
+          <SyntaxHighlighter language="json" style={dracula}>
             {jsonContent === null ? '' : JSON.stringify(jsonContent, null, 2)}
           </SyntaxHighlighter>
         </div>
-        <div className="flex justify-center gap-5">
-          <button onClick={handleSubmit} className="btn ">
-            Enviar a la API
-          </button>
+        <div className="flex justify-end gap-5">
           {/* TODO: Here goes handleDownload */}
-          <button onClick={handleCopy} className='btn'>
+          <button onClick={handleCopy} className='btn bg-yateBlue text-white'>
             Copiar JSON
           </button>
-          <button onClick={handleDownload} className="btn">
+          <button onClick={handleDownload} className="btn bg-yateBlue text-white hover:bg-white">
             Descargar JSON
           </button>
         </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,7 +9,18 @@ module.exports = {
       colors: {
         darkPurple: '#160C28',
         naplesYellow: '#EFCB68',
-        honeydew: '#E1EFE6'
+        honeydew: '#E1EFE6',
+        ghostWhite: '#F4F4F8',
+        tomato: '#FE4A49',
+        mintGreen: '#CDF7F6',
+        babypowder: '#FDFFFC',
+        yateBlue: '#083D77',
+        sandyBrown: '#EE964B',
+        beige: '#EBEBD3',
+        isabelline: '#F9F6F0',
+        resedaGreen: '#6A8D73',
+        whiteSmoke: '#F5F5F5',
+        celestialBlue: '#3E92CC'
       }
     },
   },
